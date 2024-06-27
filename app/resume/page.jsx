fa6eda7e78367cb534f8db51c50c26d01e95202d
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-06-28 00:49:59 +0530

    Add a Skill

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -113,6 +113,10 @@ const skill = {
             icon: <FaNodeJs />,
             name: "Node.Js",
         },
+        {
+            icon: "C#",
+            name: "Node.Js",
+        },
     ]
 }
 
